c4a942fc6182bbe0bbbe94c91205e99fde1a0170
Yungon Park <hahafree12@gmail.com>
2015-11-09 22:39:32 +0900

    Remove unused import

--- app/src/main/java/kr/gonigoni/gondialer/MainActivity.java
+++ app/src/main/java/kr/gonigoni/gondialer/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 import android.view.View.OnClickListener;
 
